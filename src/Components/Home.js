458a65675238c08bf81c9ff093a58bf18ba67e49
Jose <jose.muhlanga@student.moringaschool.com>
2023-03-30 19:33:00 +0300

    Some styling changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,70 +1,63 @@
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Home.css';
 
-function Home(){
-    const [books, setBooks] = useState([]);
-    
-    useEffect(() => {
-        fetch("/books")
-          .then(response => response.json()) // extract the JSON data
-          .then(data => {
-            setBooks(data); // set the state with the data
-          })
-          .catch(error => {
-            console.log(error);
-          });
-    }, []);
+function Home() {
+  const [books, setBooks] = useState([]);
 
-    
-    
-    return(
-        <>
-        <section>
-            <div className = "hero-image">
+  useEffect(() => {
+    fetch('/books')
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-                <div className = "hero-text">
+  return (
+    <div className="container">
+      <section className="hero-section">
+        <div className="hero-image">
+          <div className="hero-text">
+            <h1>Welcome to Afro Book Club</h1>
+            <p>
+              The art of being an author is a very underrated discipline, we know of some of the famous African Authors such
+              as Chinua Achebe and Chimamanda Ngozi who inspired while informing through their literal works. This sometimes
+              plays an important role of Showcasing the African Mindset while also entertaining us.
+            </p>
+            <button>Join-us</button>
+          </div>
+        </div>
+      </section>
 
-                    <h1>Welcome to Afro Book Club</h1>
-                    <p>
-                        The art of being an author is a very underrated discipline, we know of some of the famous African
-                        Authors such as Chinua Achebe and Chimamanda Ngozi who inspired while informing through
-                        their literal works. This sometimes plays an important role of Showcasing the African Mindset
-                        while also entertaining us.
-                    </p>
-                    <button>Join-us</button>
+      <section>
+        <div className="featured-books">
+          <div className="card-container">
+            {books.map((book) => (
 
+                <div key={book.id} className="card">
+                    {book.img ? (
+                        <img src={book.img} alt={book.title} onError={(e) => { e.target.onerror = null; e.target.src = "../Images/placeholder.jpg" }} />
+
+                    ) : (
+                        <div className="placeholder-image" />
+                    )}
+                    <div className="card-body">
+                        <h3>{book.title}</h3>
+                        <p>{book.user && book.user.username}</p>
+                        <Link to={`/books/${book.id}`}>Find out More</Link>
+                    </div>
                 </div>
-
-
-            </div>
-        </section>
-
-        <section>
-
-            <div className = "featured Books">
-
-                <div className="card-container">
-                    {books.map(book => (
-                        <div key={book.id} className="card">
-                            <img src={book.img} alt={book.title} />
-                            <div className="card-body">
-                            <h3>{book.title}</h3>
-                            <p>{book.user && book.user.username}</p>
-                            <button>Find out More</button>
-                            </div>
-                        </div>
-                    ))}
-
-                </div>
-
-            </div>
-
-        </section>
             
-
             
-        </>
-        
-    );
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,42 +1,72 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/NavBar.css";
+import Logo from "../Images/afrologo.jpg"; // import your logo image here
 
-function NavBar(){
+function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const userId = sessionStorage.getItem("userId");
 
   const handleLogout = () => {
     sessionStorage.clear();
-    window.location.href = '/';
-  }
+    window.location.href = "/";
+  };
+
+  const handleNavClick = () => {
+    setIsNavOpen(!isNavOpen);
+  };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/books">Books</Link>
-        </li>
-        <li>
-          <Link to="/authors">Authors</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        {userId ? (
-          <li className="dropdown">
-            <Link to="/profile">{sessionStorage.getItem("userName")} <i className="fa fa-caret-down"></i></Link>
-            <div className="dropdown-content">
-              <Link to="#" onClick={handleLogout}>Logout</Link>
-            </div>
-          </li>
-        ) : (
+    <nav className="sticky-nav">
+      <Link to="/" className="nav-logo-link">
+        <img src={Logo} alt="Afro-Book-Club Logo" className="nav-logo" />
+        <span className="nav-link-label">Afro-Book-Club</span>
+      </Link>
+
+      <div className={`nav-menu ${isNavOpen ? "nav-menu-open" : ""}`}>
+        <ul>
           <li>
-            <Link to="/login">Log-In</Link>
+            <Link to="/books" onClick={handleNavClick}>
+              Books
+            </Link>
           </li>
-        )}
-      </ul>
+          <li>
+            <Link to="/authors" onClick={handleNavClick}>
+              Authors
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={handleNavClick}>
+              About
+            </Link>
+          </li>
+          {userId ? (
+            <li className="dropdown">
+              <Link to="/profile" onClick={handleNavClick}>
+                {sessionStorage.getItem("userName")}{" "}
+                <i className="fa fa-caret-down"></i>
+              </Link>
+              <div className="dropdown-content">
+                <Link to="#" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </div>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login" onClick={handleNavClick}>
+                Log-In
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+
+      <div className="nav-toggle" onClick={handleNavClick}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
     </nav>
   );
 }
